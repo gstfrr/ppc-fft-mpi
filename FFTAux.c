9ae25643569f25006775c40d30ca4f1aff2a3f87
Augusto Ferreira <gstfrr@gmail.com>
2018-12-11 04:19:34 -0200

    iniciar entrada na classe auxiliar

--- FFTAux.c
+++ FFTAux.c
@@ -31,6 +31,38 @@ void computeEulers(Complex *Euler) {
     }
 }
 
+//***************************************************************************
+//This function define the 8 first elements of an input array as some values
+//and the rest as 0
+//***************************************************************************
+void loadData(Complex *Input) {
+
+    Input[0].real = 2.6;
+    Input[1].real = 6.3;
+    Input[2].real = 4.0;
+    Input[3].real = 9.1;
+    Input[4].real = 0.4;
+    Input[5].real = 4.8;
+    Input[6].real = 2.6;
+    Input[7].real = 4.1;
+
+    Input[0].imag = 3.6;
+    Input[1].imag = 2.9;
+    Input[2].imag = 5.6;
+    Input[3].imag = 4.8;
+    Input[4].imag = 3.3;
+    Input[5].imag = 5.9;
+    Input[6].imag = 5.0;
+    Input[7].imag = 4.3;
+
+
+    int n;
+    for (n = 8; n < N; n++) {
+        Input[n].real = Input[n].imag = 0.0;
+    }
+}
+
+
 void printHead() {
     printf("Total de valores processados: " STRONGT "%d\n\n"RESETT, N);
 }
@@ -42,7 +74,7 @@ void printResult(Complex *Result) {
 
     int k;
     for (k = 0; k < PRINT_LIMIT; k++) {
-        printf("X[%d]=\t  ( %.7f, %.7fi )\n", k, Result[k].real, Result[k].imag);
+        printf("X[%d]=\t  ( %.7f, %.7f i )\n", k, Result[k].real, Result[k].imag);
     }
 }
 
@@ -52,6 +84,6 @@ void printResultSeparate(double *ResultR, double *ResultI) {
 
     int k;
     for (k = 0; k < PRINT_LIMIT; k++) {
-        printf("X[%d]=\t  ( %.7f, %.7fi )\n", k, ResultR[k], ResultI[k]);
+        printf("X[%d]=\t  ( %.7f, %.7f i )\n", k, ResultR[k], ResultI[k]);
     }
 }
